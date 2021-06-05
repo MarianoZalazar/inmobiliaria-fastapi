28c1f3d99870aa0b2284eff00c51298ba2a52d48
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-05 13:29:40 -0300

    Actualizando variables de entorno

--- mongodb.py
+++ mongodb.py
@@ -16,8 +16,7 @@ DB = environ['DB']
 PORT = environ['PORT']
 
 client_mongo = MongoClient('mongodb+srv://' + USER + ':' + PSWD + SERVER + '/' + DB + '?' + urlencode(params))
-db = client_mongo['worshop2']
-collection = db['properati']
+collection = DB['properati']
 
 def buscar_por_barrio(barrio):
     result = [objeto for objeto in collection.find(filter={'barrio':(barrio)}, projection={'_id': 0})]
