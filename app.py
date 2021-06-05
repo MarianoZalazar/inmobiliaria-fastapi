b0ec5ef68dbc0ed117ada857a1309b2eb5d7dd5d
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-05 12:55:35 -0300

    Añadiendo API en Flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, json
-from mongodb import PORT
+import mongodb as db
 
 def get_json(dictionary):
     return app.response_class(response = json.dumps(dictionary), status = 200, mimetype = "application/json")
@@ -9,6 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello Flask"
+
+@app.route('/api/<string:barrio>')
+def barrio(barrio):
+    propiedades_por_barrio = db.buscar_por_barrio(barrio.lower())
+    return get_json(propiedades_por_barrio)
+
+@app.route('/api/<string:barrio>/<string:tipo>')
+def tipo(barrio, tipo):
+    #Query buscar por barrio y tipo
+    propiedades_por_barrio_y_tipo = db.buscar_por_barrio_y_tipo(barrio.lower(), tipo.lower())
+    return get_json(propiedades_por_barrio_y_tipo)
 
 if __name__ == '__main__':
     app.run(port = PORT, debug = True, host="0.0.0.0")
