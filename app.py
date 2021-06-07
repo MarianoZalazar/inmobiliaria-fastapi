08980a90e4d77000daaec8d8a475b866a2a46452
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-07 00:34:00 -0300

    Añadiendo ruta para busqueda de tipo

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 from flask import Flask, json
 import mongodb as db
 
-print(db.PORT)
 
 def get_json(dictionary):
-    return app.response_class(response = json.dumps(dictionary), status = 200, mimetype = "application/json")
+    return app.response_class(response=json.dumps(dictionary), status=200, mimetype="application/json")
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Hello Flask"
 
+
 @app.route('/api/<string:barrio>')
 def barrio(barrio):
-    propiedades_por_barrio = db.buscar_por_barrio(barrio.lower())
-    return get_json(propiedades_por_barrio)
+    propiedades = db.buscar_por_barrio(barrio.lower())
+    return get_json(propiedades)
 
-@app.route('/api/<string:barrio>/<string:tipo>')
-def tipo(barrio, tipo):
-    propiedades_por_barrio_y_tipo = db.buscar_por_barrio_y_tipo(barrio.lower(), tipo.lower())
-    return get_json(propiedades_por_barrio_y_tipo)
 
-#@app.route('/api/<string:barrio>/<string:inmueble>/<string:tipo>')
-#def tipo(barrio, inmueble, tipo):
-    propiedades_por_barrio_y_tipo = db.buscar_por_barrio_y_tipo(barrio.lower(), inmueble.lower(), tipo.lower())
-    return get_json(propiedades_por_barrio_y_tipo)
+@app.route('/api/<string:barrio>/<string:inmueble>')
+def barrio_inmueble(barrio, inmueble):
+    propiedades = db.buscar_por_barrio_inmueble(
+        barrio.lower(), inmueble.lower())
+    return get_json(propiedades)
+
+
+@app.route('/api/<string:barrio>/<string:inmueble>/<string:tipo>')
+def barrio_inmueble_tipo(barrio, inmueble, tipo):
+   propiedades = db.buscar_por_barrio_inmueble_tipo(
+       barrio.lower(), inmueble.lower(), tipo.lower())
+   return get_json(propiedades)
 
 
 if __name__ == '__main__':
-    app.run(port = db.PORT)
+    app.run(port=db.PORT)
