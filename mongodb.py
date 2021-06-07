6ea18976c462025ecd183985a0bb249e6718849c
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-07 00:34:21 -0300

    Añadiendo funcion para insertar anuncios

--- mongodb.py
+++ mongodb.py
@@ -18,14 +18,21 @@ PORT = environ['PORT']
 client_mongo = MongoClient('mongodb+srv://' + USER + ':' + PSWD + SERVER + '/' + DB + '?' + urlencode(params))
 collection = client_mongo[DB]['properati']
 
+def insertar_anuncios(lista_de_anuncios):
+    if lista_de_anuncios != []:
+        print('Insertando')
+        collection.insert_many(lista_de_anuncios)
+    else:
+        print("No hay resultados")
+
 def buscar_por_barrio(barrio):
     result = [objeto for objeto in collection.find(filter={'barrio':(barrio)}, projection={'_id': 0})]
     return result
 
-def buscar_por_barrio_y_tipo(barrio, tipo):
-    result = [objeto for objeto in collection.find(filter={'barrio':(barrio), 'tipo':(tipo)}, projection={'_id': 0})]
+def buscar_por_barrio_inmueble(barrio, inmueble):
+    result = [objeto for objeto in collection.find(filter={'barrio':(barrio), 'inmueble':(inmueble)}, projection={'_id': 0})]
     return result
 
-#def buscar_por_barrio_y_tipo(barrio, inmueble, tipo):
-#    result = [objeto for objeto in collection.find(filter={'barrio':(barrio), 'inmueble':(inmueble) 'tipo':(tipo)}, projection={'_id': 0})]
-#    return result
+def buscar_por_barrio_inmueble_tipo(barrio, inmueble, tipo):
+    result = [objeto for objeto in collection.find(filter={'barrio':(barrio), 'inmueble':(inmueble), 'tipo':(tipo)}, projection={'_id': 0})]
+    return result
