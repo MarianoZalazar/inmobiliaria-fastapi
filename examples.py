29290fe64e28edb05a95e7e552245571fcf76c3a
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-08 19:33:43 -0300

    Añadiendo ejemplos para documentacion

---
+++ examples.py
@@ -0,0 +1,17 @@
+post_example = {
+                "titulo": "Departamento en Palermo",
+                "inmueble": "departamento",
+                "expensas": 1200,
+                "vendedor": "aka propiedades",
+                "barrio": "palermo",
+                "tipo": "alquiler",
+                "precio_peso": 30000.0,
+                "precio_dolar": 200.0
+                }
+
+
+put_example = {
+    "titulo": "Departamento en San Nicolás",
+    "precio_peso": 100000.0,
+    "precio_dolar": 74563.12
+  }
