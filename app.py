1d3dd17b656bf29ac7d40fe3b1d9a4016df2573b
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-08 15:40:56 -0300

    Añadiendo Rutas con FastAPI

--- app.py
+++ app.py
@@ -1,38 +1,54 @@
-from flask import Flask, json
+from fastapi import FastAPI, HTTPException, Body, status
+from typing import List
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from models.anuncio import Anuncio
 import mongodb as db
 
+app = FastAPI()
 
-def get_json(dictionary):
-    return app.response_class(response=json.dumps(dictionary), status=200, mimetype="application/json")
+@app.get('/')
+async def root():
+    return {"msg": "Root"}
+
+@app.get('/api/{barrio_o_tipo}', response_model=List[Anuncio])
+async def get_by_hood_or_type(barrio_o_tipo: str):
+    if barrio_o_tipo == 'venta' or barrio_o_tipo == 'alquiler':
+        lista_anuncios = await db.buscar_por_tipo(barrio_o_tipo)
+    else:
+        lista_anuncios = await db.buscar_por_barrio(barrio_o_tipo)
+        
+    if len(lista_anuncios) >= 1:
+        return lista_anuncios
+    
+    raise HTTPException(status_code=404, detail=f"{barrio_o_tipo} not found")
+
+@app.get('/api/{barrio}/{inmueble}', response_model=List[Anuncio])
+async def get_by_property_type(barrio: str, inmueble: str):
+    lista_anuncios = await db.buscar_por_barrio_inmueble(barrio.lower(), inmueble.lower())
+    if len(lista_anuncios)>=1:
+        return lista_anuncios
+    
+    raise HTTPException(status_code=404, detail=f"Resource not found")
+    
+@app.get('/api/{barrio}/{inmueble}/{tipo}', response_model=List[Anuncio])
+async def get_by_property_type_and_rent_sold(barrio: str, inmueble: str, tipo: str):
+    lista_anuncios = await db.buscar_por_barrio_inmueble_tipo(barrio.lower(), inmueble.lower(), tipo.lower())
+    if len(lista_anuncios)>=1:
+        return lista_anuncios
+    
+    raise HTTPException(status_code=404, detail=f"Resource not found")
+
+@app.post('/api/propiedades', response_model=Anuncio)
+async def post_property(anuncio: Anuncio = Body(...)):
+    anuncio = jsonable_encoder(anuncio)
+    new_anuncio = await db.insertar_anuncio(anuncio)
+    return JSONResponse(content=new_anuncio, status_code=status.HTTP_201_CREATED)
 
 
-app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return "Hello Flask"
-
-
-@app.route('/api/<string:barrio>')
-def barrio(barrio):
-    propiedades = db.buscar_por_barrio(barrio.lower())
-    return get_json(propiedades)
-
-
-@app.route('/api/<string:barrio>/<string:inmueble>')
-def barrio_inmueble(barrio, inmueble):
-    propiedades = db.buscar_por_barrio_inmueble(
-        barrio.lower(), inmueble.lower())
-    return get_json(propiedades)
-
-
-@app.route('/api/<string:barrio>/<string:inmueble>/<string:tipo>')
-def barrio_inmueble_tipo(barrio, inmueble, tipo):
-   propiedades = db.buscar_por_barrio_inmueble_tipo(
-       barrio.lower(), inmueble.lower(), tipo.lower())
-   return get_json(propiedades)
-
-
-if __name__ == '__main__':
-    app.run(port=db.PORT)
+#Fix ruta api/barrio_o_tipo (probable error en scrapeo)
+#Añadir campo ID (ObjectdID o UUID)
+#PUT-DELETE Methods
+#Añadir query parameters (limits, dates)
