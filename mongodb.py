619a7c2fb012ea2239f9de5a13999d0e4b94fd0a
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-08 19:34:29 -0300

    Añadiendo mas funciones para insercion, actualizacion y eliminacion de datos

--- mongodb.py
+++ mongodb.py
@@ -2,6 +2,7 @@ import motor.motor_asyncio as motor
 import settings
 from os import environ
 from urllib.parse import urlencode
+from pymongo import ReturnDocument
 
 params = {'retryWrites': 'true',
           'w': 'majority',
@@ -22,34 +23,44 @@ collection = bd['propiedades']
 
 #Usar for loops en vez de list comprehension para mejor entendimiento
 
+def insertar_varios_anuncios(lista_de_anuncios):
+    collection.insert_many(lista_de_anuncios)
+
+
 async def buscar_por_tipo(tipo):
     lista_de_anuncios = []
-    async for anuncio in collection.find(filter={'tipo': tipo},projection={'_id': 0}):
+    async for anuncio in collection.find(filter={'tipo': tipo}):
         lista_de_anuncios.append(anuncio)
     return lista_de_anuncios
 
 async def buscar_por_barrio(barrio):
     lista_de_anuncios = []
-    async for anuncio in collection.find(filter={'barrio': barrio},
-                                         projection={'_id': 0}):
+    async for anuncio in collection.find(filter={'barrio': barrio}):
         lista_de_anuncios.append(anuncio)
     return lista_de_anuncios
 
 async def buscar_por_barrio_inmueble(barrio, inmueble):
     lista_de_anuncios = []
-    async for anuncio in collection.find(filter={'barrio': barrio, 'inmueble': inmueble},
-                                         projection={'_id': 0}):
+    async for anuncio in collection.find(filter={'barrio': barrio, 'inmueble': inmueble}):
         lista_de_anuncios.append(anuncio)
     return lista_de_anuncios
 
 async def buscar_por_barrio_inmueble_tipo(barrio, inmueble, tipo):
     lista_de_anuncios = []
-    async for anuncio in collection.find(filter={'barrio': barrio, 'inmueble': inmueble, 'tipo': tipo},
-                                         projection={'_id': 0}):
+    async for anuncio in collection.find(filter={'barrio': barrio, 'inmueble': inmueble, 'tipo': tipo}):
         lista_de_anuncios.append(anuncio)
     return lista_de_anuncios
 
 async def insertar_anuncio(anuncio):
     new_anuncio = await collection.insert_one(anuncio)
-    anuncio_creado = await collection.find_one(filter={"_id": new_anuncio.inserted_id}, projection={'_id': 0})
+    anuncio_creado = await collection.find_one(filter={"_id": new_anuncio.inserted_id})
     return anuncio_creado
+
+
+async def actualizar_anuncio(id, anuncio):
+    anuncio_actualizado = await collection.find_one_and_update({'_id': id}, {'$set': anuncio}, return_document=ReturnDocument.AFTER)
+    return anuncio_actualizado
+
+async def eliminar_anuncio(id):
+    anuncio_eliminado = await collection.delete_one({'_id': id})
+    return anuncio_eliminado
