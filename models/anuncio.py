91ba0e1cbe3468f9394e49d3a5f4995cc5e49bdd
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-08 19:31:18 -0300

    Agregando nuevo modelo para metodo Put y configurando ObjectID

--- models/anuncio.py
+++ models/anuncio.py
@@ -3,16 +3,17 @@ from typing import Optional, List
 from datetime import datetime
 from unidecode import unidecode
 from barrios import lista_barrios
-#from pyobjectid import PydanticObjectId
+from models.pyobjectid import PydanticObjectId
+from bson import ObjectId
 
 class Anuncio(BaseModel):
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias='_id')
     titulo: str = Field(...)
     fecha_publicacion: Optional[datetime] = datetime.now()
     inmueble: str = Field(...)
     expensas: float = Field(None)
-    #Implementar tipo User
     vendedor: str = Field(...)
-    barrio: str = Field(...)
+    barrio: str = Field(None)
     tipo: str = Field(...)
     precio_peso: float = Field(...)
     #Buscar forma para autogestionar precio_dolar
@@ -27,8 +28,56 @@ class Anuncio(BaseModel):
         if barrio in lista_barrios:
             return barrio
         else:
-            raise ValueError('Barrio no corresponde')
+            raise ValueError(f'{barrio} no corresponde')
     
     @validator('precio_peso', 'precio_dolar')
     def round_result(cls, v):
         return round(v, 2)
+    
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "_id": "60bf5114071947dsa145d46bb2",
+                "titulo": "Departamento en Caballito",
+                "fecha_publicacion": "2021-06-07T00:00:00",
+                "inmueble": "departamento",
+                "expensas": 10000,
+                "vendedor": "cortese propiedades",
+                "barrio": "caballito",
+                "tipo": "venta",
+                "precio_peso": 14347190.0,
+                "precio_dolar": 152321.8
+        }}
+        
+class Anuncio_actualizar(BaseModel):
+    titulo: Optional[str]
+    fecha_publicacion: Optional[datetime] = datetime.now()
+    inmueble: Optional[str]
+    expensas: Optional[float]
+    vendedor: Optional[str]
+    barrio: Optional[str]
+    tipo: Optional[str]
+    precio_peso: Optional[float]
+    precio_dolar: Optional[float]
+    
+    @validator('inmueble', 'barrio', 'tipo')
+    def lowercase_strings(cls, v):
+        return unidecode(v.lower())
+    
+    @validator('barrio')
+    def verificar_barrio(cls, barrio):
+        if barrio in lista_barrios:
+            return barrio
+        else:
+            raise ValueError(f'{barrio} no corresponde')
+    
+    @validator('precio_peso', 'precio_dolar')
+    def round_result(cls, v):
+        return round(v, 2)
+    
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
--- models/pyobjectid.py
+++ models/pyobjectid.py
@@ -10,3 +10,7 @@ class PydanticObjectId(ObjectId):
         if not ObjectId.is_valid(v):
             raise ValueError("Invalid objectid")
         return ObjectId(v)
+    
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
