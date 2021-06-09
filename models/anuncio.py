a02734da19ffac19e1736cfa71e64f956814c8eb
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-09 14:04:30 -0300

    Modificando campos y añadiendo mas validaciones

--- models/anuncio.py
+++ models/anuncio.py
@@ -6,34 +6,41 @@ from barrios import lista_barrios
 from models.pyobjectid import PydanticObjectId
 from bson import ObjectId
 
+
 class Anuncio(BaseModel):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias='_id')
     titulo: str = Field(...)
-    fecha_publicacion: Optional[datetime] = datetime.now()
+    fecha_publicacion: datetime = Field(datetime.now())
     inmueble: str = Field(...)
-    expensas: float = Field(None)
     vendedor: str = Field(...)
-    barrio: str = Field(None)
+    barrio: str = Field(...)
     tipo: str = Field(...)
-    precio_peso: float = Field(...)
-    #Buscar forma para autogestionar precio_dolar
-    precio_dolar: float = Field(...)
-    
+    moneda: str = Field(...)
+    precio: float = Field(..., gt=0)
+    expensas: float = Field(None)
+
     @validator('inmueble', 'barrio', 'tipo')
     def lowercase_strings(cls, v):
         return unidecode(v.lower())
-    
+
     @validator('barrio')
     def verificar_barrio(cls, barrio):
         if barrio in lista_barrios:
             return barrio
         else:
             raise ValueError(f'{barrio} no corresponde')
-    
-    @validator('precio_peso', 'precio_dolar')
+
+    @validator('moneda')
+    def verificar_moneda(cls, moneda):
+        if moneda == 'ARS' or moneda == 'USD':
+            return moneda
+        else:
+            raise ValueError(f'{moneda} no corresponde')
+
+    @validator('precio')
     def round_result(cls, v):
         return round(v, 2)
-    
+
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
@@ -48,36 +55,46 @@ class Anuncio(BaseModel):
                 "vendedor": "cortese propiedades",
                 "barrio": "caballito",
                 "tipo": "venta",
-                "precio_peso": 14347190.0,
-                "precio_dolar": 152321.8
-        }}
-        
+                "moneda": "ARS",
+                "precio": 14347190.0
+            }}
+
+
 class Anuncio_actualizar(BaseModel):
     titulo: Optional[str]
-    fecha_publicacion: Optional[datetime] = datetime.now()
     inmueble: Optional[str]
     expensas: Optional[float]
     vendedor: Optional[str]
     barrio: Optional[str]
     tipo: Optional[str]
-    precio_peso: Optional[float]
-    precio_dolar: Optional[float]
-    
+    moneda: Optional[str]
+    precio: Optional[float]
+
     @validator('inmueble', 'barrio', 'tipo')
     def lowercase_strings(cls, v):
         return unidecode(v.lower())
-    
+
     @validator('barrio')
     def verificar_barrio(cls, barrio):
         if barrio in lista_barrios:
             return barrio
         else:
             raise ValueError(f'{barrio} no corresponde')
-    
-    @validator('precio_peso', 'precio_dolar')
+
+    @validator('moneda')
+    def verificar_moneda(cls, moneda):
+        if moneda == 'ARS' or moneda == 'USD':
+            return Moneda
+        else:
+            raise ValueError(f'{moneda} no corresponde')
+
+    @validator('precio')
     def round_result(cls, v):
-        return round(v, 2)
-    
+        if v > 0:
+            return round(v, 2)
+        else:
+            raise ValueError('Precio debe ser mayor a 0')
+
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
