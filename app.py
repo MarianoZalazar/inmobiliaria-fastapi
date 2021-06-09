a027e9abc6923d01c6a63447939cbf229178ea18
MarianoZalazar <marianoz.1507@gmail.com>
2021-06-09 16:45:50 -0300

    Corrigiendo error en metodo DELETE

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ app = FastAPI(title="Inmobiliaria API",
               )
 
 
-
-
 @app.get('/', include_in_schema=False)
 async def root():
     response = RedirectResponse('/docs')
@@ -23,63 +21,70 @@ async def root():
 
 ##############################################################################
 
-@app.get('/api/{barrio_o_tipo}', 
-         response_description = 'Obtener lista de anuncios por barrio o tipo de operacion', 
+
+@app.get('/api/{barrio_o_tipo}',
+         response_description='Obtener lista de anuncios por barrio o tipo de operacion',
          response_model=List[Anuncio])
 async def get_by_barrio_o_tipo(barrio_o_tipo: str = Path(..., title="Un barrio o tipo de operacion",
                                                          examples={
-                                                            "Barrio": {"value": "las-canitas"},
-                                                            "Tipo": {"value": "venta"}
+                                                             "Barrio": {"value": "las-canitas"},
+                                                             "Tipo": {"value": "venta"}
                                                          }),
-                                q: CommonQueryModel = Depends(CommonQueryModel)
-                                ):
+                               q: CommonQueryModel = Depends(CommonQueryModel)
+                               ):
     if barrio_o_tipo == 'venta' or barrio_o_tipo == 'alquiler':
         lista_anuncios = await db.buscar_anuncios({'tipo': barrio_o_tipo.lower()}, q)
     else:
         lista_anuncios = await db.buscar_anuncios({'barrio': barrio_o_tipo.lower()}, q)
-        
+
     if len(lista_anuncios) >= 1:
         return lista_anuncios
-    
+
     raise HTTPException(status_code=404, detail=f"{barrio_o_tipo} not found")
 
 ##############################################################################
 
-@app.get('/api/{barrio}/{inmueble}', 
-         response_description = 'Obtener lista de anuncios por barrio e inmueble', 
+
+@app.get('/api/{barrio}/{inmueble}',
+         response_description='Obtener lista de anuncios por barrio e inmueble',
          response_model=List[Anuncio])
-async def get_by_barrio_inmueble(barrio: str = Path(..., 
-                                                    title="Nombre del barrio, intercambiar espacios con '-' ", 
-                                                    example="nunez"), 
-                                 inmueble: str = Path(..., title="Tipo de inmueble", example="departamento"),
+async def get_by_barrio_inmueble(barrio: str = Path(...,
+                                                    title="Nombre del barrio, intercambiar espacios con '-' ",
+                                                    example="nunez"),
+                                 inmueble: str = Path(..., title="Tipo de inmueble",
+                                                      example="departamento"),
                                  q: CommonQueryModel = Depends(CommonQueryModel)):
     lista_anuncios = await db.buscar_anuncios({'barrio': barrio.lower(), 'inmueble': inmueble.lower()}, q)
-    if len(lista_anuncios)>=1:
+    if len(lista_anuncios) >= 1:
         return lista_anuncios
-    
+
     raise HTTPException(status_code=404, detail=f"Resource not found")
 
 ##############################################################################
-    
-@app.get('/api/{barrio}/{inmueble}/{tipo}', 
-         response_description = 'Obtener lista de anuncios por barrio, inmueble y tipo de operacion', 
+
+
+@app.get('/api/{barrio}/{inmueble}/{tipo}',
+         response_description='Obtener lista de anuncios por barrio, inmueble y tipo de operacion',
          response_model=List[Anuncio])
-async def get_by_barrio_inmueble_tipo(barrio: str = Path(..., 
-                                                    title="Nombre del barrio, intercambiar espacios con '-' ", 
-                                                    example="san-nicolas"), 
-                                      inmueble: str = Path(..., title="Tipo de inmueble", example="casa"), 
-                                      tipo: str = Path(..., title="Tipo de operacion alquiler/venta", example="alquiler"),
+async def get_by_barrio_inmueble_tipo(barrio: str = Path(...,
+                                                         title="Nombre del barrio, intercambiar espacios con '-' ",
+                                                         example="san-nicolas"),
+                                      inmueble: str = Path(
+                                          ..., title="Tipo de inmueble", example="casa"),
+                                      tipo: str = Path(
+                                          ..., title="Tipo de operacion alquiler/venta", example="alquiler"),
                                       q: CommonQueryModel = Depends(CommonQueryModel)):
     lista_anuncios = await db.buscar_anuncios({'barrio': barrio.lower(), 'inmueble': inmueble.lower(), 'tipo': tipo.lower()}, q)
-    if len(lista_anuncios)>=1:
+    if len(lista_anuncios) >= 1:
         return lista_anuncios
-    
+
     raise HTTPException(status_code=404, detail=f"Resource not found")
 
 ##############################################################################
 
-@app.post('/api/propiedades', 
-          response_description = 'Publicar un anuncio', 
+
+@app.post('/api/propiedades',
+          response_description='Publicar un anuncio',
           response_model=Anuncio)
 async def publicar_anuncio(anuncio: Anuncio = Body(..., example=examples.post_example)):
     anuncio = jsonable_encoder(anuncio)
@@ -88,10 +93,11 @@ async def publicar_anuncio(anuncio: Anuncio = Body(..., example=examples.post_ex
 
 ##############################################################################
 
-@app.put('/api/propiedades/{id}', 
-         response_description = 'Actualizar un anuncio', 
+
+@app.put('/api/propiedades/{id}',
+         response_description='Actualizar un anuncio',
          response_model=Anuncio)
-async def actualizar_anuncio(id: str = Path(..., example='fd23114h2y0719d46ba9'), 
+async def actualizar_anuncio(id: str = Path(..., example='fd23114h2y0719d46ba9'),
                              anuncio: Anuncio_actualizar = Body(..., example=examples.put_example)):
     anuncio = {k: v for k, v in anuncio.dict().items() if v is not None}
     if len(anuncio) >= 1:
@@ -103,13 +109,12 @@ async def actualizar_anuncio(id: str = Path(..., example='fd23114h2y0719d46ba9')
 
 ##############################################################################
 
-@app.delete('/api/propiedades/{id}', 
-            response_description = 'Eliminar un anuncio')
+
+@app.delete('/api/propiedades/{id}',
+            response_description='Eliminar un anuncio')
 async def borrar_anuncio(id: str = Path(..., example='fd23114h2y0719d46ba9')):
-    anuncio_eliminado = db.eliminar_anuncio(id)
+    anuncio_eliminado = await db.eliminar_anuncio(id)
     if anuncio_eliminado.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
     raise HTTPException(status_code=404, detail=f"Anuncio {id} not found")
 
-
-#Añadir query parameters (limits, dates)
